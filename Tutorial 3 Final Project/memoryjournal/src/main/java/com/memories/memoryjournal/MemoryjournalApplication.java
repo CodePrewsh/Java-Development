e2dce93a1e8b8557718216a8718abf51b4fdc8b0
Precious Thandeka Bhembe <91918136+CodePrewsh@users.noreply.github.com>
2025-06-20 21:31:46 +0200

    Add files via upload

---
+++ Tutorial 3 Final Project/memoryjournal/src/main/java/com/memories/memoryjournal/MemoryjournalApplication.java
@@ -0,0 +1,13 @@
+package com.memories.memoryjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryjournalApplication.class, args);
+	}
+
+}
